dedc28bc54219a8f7d264d6ef44e3efcc33d9659
suraikin-ilya <ilya.suraikin@mail.ru>
2021-05-13 15:29:15 +0300

    admin

--- rent_car/main/admin.py
+++ rent_car/main/admin.py
@@ -1,3 +1,212 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand, Transmission, Color, Status, Car, Photo, Client, Contact, OrderStatus, Order
+from django.db.models.functions import Lower
+from import_export.admin import ImportExportModelAdmin
+from import_export.formats import base_formats
+
+
+@admin.register(Car)
+class CarAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ("car_id", "brand", "price", "car_status", "color_name", "type")
+    search_fields = ['brand', 'color_name']
+    list_filter = ['car_status', 'price']
+    pass
+
+    def get_ordering(self, request):
+        return [Lower('car_id')]
+
+    def get_import_formats(self):
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_import()]
+
+    def get_export_formats(self):
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_export()]
+
+
+@admin.register(Brand)
+class BrandAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('brand_id', "brand", "description")
+    search_fields = ['brand', 'description']
+    list_filter = ['brand_id', 'brand']
+    pass
+
+    def get_ordering(self, request):
+        return [Lower('brand_id')]
+
+    def get_import_formats(self):
+        formats = (
+                  base_formats.XLS,
+                  base_formats.XLSX,
+            )
+        return [f for f in formats if f().can_import()]
+
+    def get_export_formats(self):
+        formats = (
+                  base_formats.XLS,
+                  base_formats.XLSX,
+            )
+        return [f for f in formats if f().can_export()]
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('status_id', "car_status")
+    list_filter = ['status_id', 'car_status']
+    pass
+
+    def get_ordering(self, request):
+        return [Lower('status_id')]
+
+
+@admin.register(Transmission)
+class TransmissionAdmin(admin.ModelAdmin):
+    list_display = ('transmission_id', "type")
+    list_filter = ['transmission_id', 'type']
+    pass
+
+    def get_ordering(self, request):
+        return [Lower('transmission_id')]
+
+
+@admin.register(Color)
+class ColorAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('color_id', "color_name")
+    list_filter = ['color_id', 'color_name']
+    search_fields = ['color_name']
+    pass
+
+    def get_ordering(self, request):
+        return [Lower('color_id')]
+
+    def get_import_formats(self):
+        formats = (
+                  base_formats.XLS,
+                  base_formats.XLSX,
+            )
+        return [f for f in formats if f().can_import()]
+
+    def get_export_formats(self):
+        formats = (
+                  base_formats.XLS,
+                  base_formats.XLSX,
+            )
+        return [f for f in formats if f().can_export()]
+
+
+@admin.register(Photo)
+class PhotoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('car_id', "photo_id", "url")
+    search_fields = ['photo_id']
+    list_filter = ['photo_id']
+    pass
+
+    def get_ordering(self, request):
+        return [Lower('photo_id')]
+
+    def get_import_formats(self):
+        formats = (
+                  base_formats.XLS,
+                  base_formats.XLSX,
+            )
+        return [f for f in formats if f().can_import()]
+
+    def get_export_formats(self):
+        formats = (
+                  base_formats.XLS,
+                  base_formats.XLSX,
+            )
+        return [f for f in formats if f().can_export()]
+
+
+@admin.register(OrderStatus)
+class OrderStatusAdmin(admin.ModelAdmin):
+    list_display = ('order_id', "order_status")
+    list_filter = ['order_id', 'order_status']
+    pass
+
+    def get_ordering(self, request):
+        return [Lower('order_id')]
+
+
+@admin.register(Order)
+class OrderAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ("order_id", "client_id", "car", "order_status")
+    search_fields = ['car']
+    list_filter = ['car', 'order_id']
+    pass
+
+    def get_ordering(self, request):
+        return [Lower('order_id')]
+
+    def get_import_formats(self):
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_import()]
+
+    def get_export_formats(self):
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_export()]
+
+
+@admin.register(Client)
+class ClientAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ("client_id", "name", "surname")
+    search_fields = ['name', 'surname']
+    list_filter = ['name', 'surname', 'client_id']
+    pass
+
+    def get_ordering(self, request):
+        return [Lower('client_id')]
+
+    def get_import_formats(self):
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_import()]
+
+    def get_export_formats(self):
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_export()]
+
+
+@admin.register(Contact)
+class ContactAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ("client_id", "phone", "e_mail", 'address')
+    search_fields = ['phone', 'e_mail', 'address']
+    list_filter = ['phone', 'e_mail', 'address', 'client_id']
+    pass
+
+    def get_ordering(self, request):
+        return [Lower('client_id')]
+
+    def get_import_formats(self):
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_import()]
+
+    def get_export_formats(self):
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_export()]
+
--- rent_car/main/models.py
+++ rent_car/main/models.py
@@ -4,36 +4,65 @@ from django.db import models
 class Brand(models.Model):
     brand_id = models.AutoField(primary_key=True)
     brand = models.CharField("Марка автомобиля", max_length=50)
+    models.CharField("Модель", max_length=50)
     description = models.TextField("Описание")
 
+    class Meta:
+        verbose_name = ("Модель")
+        verbose_name_plural = ("Модели")
 
-TYPE_CHOICE = (
-    ('a', 'Автомат'),
-    ('m', "Механика"),
-    ('r', 'Роботизированная'),
-    ('v', 'Вариативная')
-)
+    def __str__(self):
+        return str(self.brand)
+
+
+# TYPE_CHOICE = (
+#     ('a', 'Автомат'),
+#     ('m', "Механика"),
+#     ('r', 'Роботизированная'),
+#     ('v', 'Вариативная')
+# )
 
 
 class Transmission(models.Model):
     transmission_id = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=1, choices=TYPE_CHOICE)
+    type = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = ("Трансмиссия")
+        verbose_name_plural = ("Трансмиссии")
+
+    def __str__(self):
+        return self.type
 
 
 class Color(models.Model):
     color_id = models.AutoField(primary_key=True)
-    color_name = models.TextField("Название цвета")
+    color_name = models.CharField("Цвет", max_length=50)
+
+    class Meta:
+        verbose_name = ("Цвет")
+        verbose_name_plural = ("Цвета")
+
+    def __str__(self):
+        return self.color_name
 
 
-CAR_STATUS_CHOICE = (
-    ('z', 'Забронирована'),
-    ('d', "Доступна для аренды"),
-)
+# CAR_STATUS_CHOICE = (
+#     ('z', 'Забронирована'),
+#     ('d', "Доступна для аренды"),
+# )
 
 
 class Status(models.Model):
     status_id = models.AutoField(primary_key=True)
-    car_status = models.CharField(max_length=1, choices=CAR_STATUS_CHOICE)
+    car_status = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = ("Статус")
+        verbose_name_plural = ("Статусы")
+
+    def __str__(self):
+        return self.car_status
 
 
 class Car(models.Model):
@@ -43,9 +72,13 @@ class Car(models.Model):
     price = models.IntegerField("Стоимость аренды")
     color_name = models.ForeignKey(Color, on_delete=models.DO_NOTHING)
     type = models.ForeignKey(Transmission, on_delete=models.DO_NOTHING)
+
     class Meta:
         verbose_name = ("Автомобиль")
         verbose_name_plural = ("Автомобили")
+
+    def __str__(self):
+        return str(self.brand)
 
 
 class Photo(models.Model):
@@ -53,31 +86,59 @@ class Photo(models.Model):
     car_id = models.ForeignKey(Car, on_delete=models.CASCADE)
     url = models.TextField("url")
 
+    class Meta:
+        verbose_name = ("Фото")
+        verbose_name_plural = ("Фото")
+
+    def __str__(self):
+        return str(self.car_id)
+
 
 class Client(models.Model):
     client_id = models.AutoField(primary_key=True)
-    name = models.TextField("Имя")
-    surname = models.TextField("Фамилия")
+    name = models.CharField("Имя", max_length=100)
+    surname = models.CharField("Фамилия", max_length=100)
+
+    class Meta:
+        verbose_name = ("Клиент")
+        verbose_name_plural = ("Клиенты")
+
+    def __str__(self):
+        return str(self.name + " " + self.surname)
 
 
 class Contact(models.Model):
     client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
-    phone = models.TextField("Телефон")
-    e_mail = models.TextField("e-mail")
-    address = models.TextField("Адресс")
+    phone = models.CharField("Телефон", max_length=50)
+    e_mail = models.CharField("e-mail", max_length=50)
+    address = models.CharField("Адрес", max_length=100)
+
+    class Meta:
+        verbose_name = ("Контакт")
+        verbose_name_plural = ("Контакты")
+
+    def __str__(self):
+        return str(self.client_id)
 
 
-ORDER_STATUS_CHOICE = (
-    ('v', 'Выполнен'),
-    ('o', "Отклонён"),
-    ('d', "Ожидает оплаты"),
-    ('n', "На рассмотрении"),
-)
+# ORDER_STATUS_CHOICE = (
+#     ('v', 'Выполнен'),
+#     ('o', "Отклонён"),
+#     ('d', "Ожидает оплаты"),
+#     ('n', "На рассмотрении"),
+# )
 
 
 class OrderStatus(models.Model):
     order_id = models.AutoField(primary_key=True)
-    order_status = models.CharField(max_length=1, choices=ORDER_STATUS_CHOICE)
+    order_status = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = ("Статус заказа")
+        verbose_name_plural = ("Статусы заказа")
+
+    def __str__(self):
+        return self.order_status
 
 
 class Order(models.Model):
@@ -85,6 +146,10 @@ class Order(models.Model):
     client_id = models.ForeignKey(Client, on_delete=models.DO_NOTHING)
     car = models.ForeignKey(Car, on_delete=models.DO_NOTHING)
     order_status = models.ForeignKey(OrderStatus, on_delete=models.CASCADE)
+
     class Meta:
         verbose_name = ("Заказ")
         verbose_name_plural = ("Заказы")
+
+    def __str__(self):
+        return str(self.order_status)
